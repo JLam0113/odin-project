27a6907e1934c43cd5cc85274acef1be02cc6301
Jessye Lam <jessyel13@gmail.com>
2024-01-19 15:59:32 -0500

    completed ui

--- rock-paper-scissors/rps.js
+++ rock-paper-scissors/rps.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let rndInt = Math.floor(Math.random() * 3) + 1;
     if (rndInt == 1) {
@@ -14,34 +17,86 @@ function playRound(playerSelection, computerSelection) {
     while (lowerCase == computerSelection) {
         computerSelection = getComputerChoice();
     }
-    console.log(computerSelection);
+    const selection = document.querySelector('#selection');
+
+    selection.innerHTML = "Player selected: " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " and Computer selected: " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
     if (lowerCase == "rock") {
-        if (computerSelection == "scissors") return 1;
-        else if (computerSelection == "paper") return 0;
+        if (computerSelection == "scissors") {
+            winRound();
+            return 1;
+        }
+        else if (computerSelection == "paper") {
+            loseRound();
+            return 0;
+        }
     }
     else if (lowerCase == "paper") {
-        if (computerSelection == "rock") return 1;
-        else if (computerSelection == "scissors") return 0;
+        if (computerSelection == "rock") {
+            winRound();
+            return 1;
+        }
+        else if (computerSelection == "scissors") {
+            loseRound();
+            return 0;
+        }
     }
     else if (lowerCase == "scissors") {
-        if (computerSelection == "paper") return 1;
-        else if (computerSelection == "rock") return 0;
+        if (computerSelection == "paper") {
+            winRound();
+            return 1;
+        }
+        else if (computerSelection == "rock") {
+            loseRound();
+            return 0;
+        }
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    while (playerScore != 3 && computerScore != 3) {
-        let playerSelection = prompt("Player Score = " + playerScore + "\nComputer Score = " + computerScore + "\nPlease enter Rock, Paper, or Scissors").toLowerCase();
-        while (["rock", "paper", "scissors"].indexOf(playerSelection) === -1)
-            playerSelection = prompt("Player Score = " + playerScore + "\nComputer Score = " + computerScore + "\nPlease enter Rock, Paper, or Scissors").toLowerCase();
-        if(playRound(playerSelection, getComputerChoice()) == 1) playerScore++;
-        else computerScore++;
-    }
-    if (playerScore == 3) console.log("You win");
-    else console.log("You lose");
-    console.log("Final score is \nPlayer Score = " + playerScore + "\nComputer Score = " + computerScore);
+function winRound() {
+    const gameResult = document.querySelector('#gameResult');
+    gameResult.innerHTML = "You've won the round";
 }
 
-game();
+function loseRound() {
+    const gameResult = document.querySelector('#gameResult');
+    gameResult.innerHTML = "You've lost the round";
+}
+
+function game(playerSelection) {
+
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+
+    if (playRound(playerSelection, getComputerChoice()) == 1) playerScore++;
+    else computerScore++;
+
+    updateScore();
+
+    if (playerScore == 5 || computerScore == 5) {
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+    }
+    else {
+        rockBtn.disabled = false;
+        paperBtn.disabled = false;
+        scissorsBtn.disabled = false;
+    }
+}
+
+function updateScore() {
+    const playerScoreText = document.querySelector('#playerScore');
+    const computerScoreText = document.querySelector('#computerScore');
+    const winner = document.querySelector('#winner');
+
+    playerScoreText.innerHTML = "Player Score: " + playerScore;
+    computerScoreText.innerHTML = "Computer Score: " + computerScore;
+
+    if (playerScore == 5) {
+        winner.innerHTML = "Congratulations, you've won!";
+    }
+    else if (computerScore == 5) {
+        winner.innerHTML = "Unfortunately, you've lost.";
+    }
+}
